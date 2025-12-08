204ef1a295dbed0571f6a02c15e27aaf7d43eb77
Yaks228 <yakasmuz@gmail.com>
2025-12-09 02:01:56 +0300

    Add files via upload

--- links.js
+++ links.js
@@ -3,7 +3,7 @@
 
 const LINKS = {
     // Ссылка на Telegram бот
-    telegram: 'https://t.me/Lucky_Bear_SpinPro_Bot/app?startapp=bXN0PTB4MWFlMzg3YWJlJm09cmF1emVkMiZjPXNlbw',
+    telegram: 'https://t.me/+kC24BeajpLgxNzI6',
     
     // Ссылка на сайт казино
     casino: 'https://lbgame777.xyz/28UtaT'
