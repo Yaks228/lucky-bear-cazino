3efdb17c5478ad8a87efd1c2d17262a921e7e66e
Yaks228 <yakasmuz@gmail.com>
2026-01-14 14:40:51 +0300

    Add files via upload

--- links.js
+++ links.js
@@ -6,6 +6,9 @@ const LINKS = {
     telegram: 'https://t.me/+7y1O8ZFFIzhkN2Yy',
     
     // Ссылка на сайт казино
-    casino: 'https://lbgame777.xyz/28UtaT'
+    casino: 'https://lbgame777.xyz/28UtaT',
+
+    // Ссылка на установку VPN (замените при необходимости)
+    vpn: 'https://t.me/VPN_OpenWorld_bot?start=NTcyNTIyNTQwMA' // Cloudflare WARP (пример)
 };
 
